55c0382c884066a02c9bfaf048c76f453ad4ef16
edwingd1 <edwingd1@gmail.com>
2025-06-04 15:12:17 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,23 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("lectura-cache").then(function(cache) {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./script.js",
-        "./manifest.json"
-      ]);
-    })
+const CACHE_NAME = 'registro-offline-cache-v3';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/manifest.json',
+  '/icon.png',
+  '/libs/html5-qrcode.min.js',
+  '/libs/xlsx.full.min.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
